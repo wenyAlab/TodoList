1e7d1a039450702aeceb5521d7d9367b8504b71e
weny <weny@annyandandy.com>
2018-03-15 18:53:40 +0800

    src目录,源文件

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todolist from '../component/todoList.js';
+import Todolistinput from '../component/todoListInput.js';
+import Todolistitem from '../component/todoListItem.js';
+import Todolistbox from '../component/todoListBox.js';
+import '../style/todoList.css';
+
+
+class App extends React.Component{
+    render(){
+        return(
+            
+                <Todolistbox/>
+            
+        )
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
